c7c7499694660270fe15d19217c9157dd78608d4
n3ey <vojta.kala.it@gmail.com>
2023-11-19 19:13:43 +0100

    feat(deps): NS-2 add handlebars

--- src/main.js
+++ src/main.js
@@ -1,14 +1,36 @@
 import './style.css'
+import Handlebars from 'handlebars';
 import formHTML from './html/form.html?raw';
 import successMsg from './html/success-mgs.html?raw';
+import iconList from './assets/images/icon-list.svg';
+import iconSuccess from './assets/images/icon-success.svg';
+import desktopImg from './assets/images/illustration-sign-up-desktop.svg';
+import mobileImg from './assets/images/illustration-sign-up-mobile.svg';
 
 const app = document.querySelector('#app');
+
+const formTemplate = Handlebars.compile(formHTML);
+const title = 'Stay Updated!';
+const info = 'Join 60,000+ product managers receiving monthly\nupdates on:';
+const placeholderText = 'email@company.com';
+const btnText = 'Subscribe to monthly newsletter';
+
+const formData = {
+    title,
+    info,
+    placeholderText,
+    btnText,
+    images: {
+        iconList,
+        desktopImg,
+    }
+}
 
 loadApp();
 function loadApp() {
     let showForm = true;
 
-    app.innerHTML = formHTML;
+    app.innerHTML = formTemplate(formData);
     const subsForm = document.querySelector('#subscriptionForm');
 
     subsForm.addEventListener('submit', e => {
